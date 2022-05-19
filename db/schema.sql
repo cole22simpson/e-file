0f0f27a5220e15aae076bc0d079a0040d3e86239
Cole Simpson <cole22simpson@gmail.com>
2022-05-19 01:11:08 -0700

    finished functions and queries

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,29 @@
 DROP DATABASE IF EXISTS company;
 CREATE DATABASE company;
-
 USE company;
+
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS departments;
+
+CREATE TABLE departments (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  dept_name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE roles (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  job_title VARCHAR(50) NOT NULL,
+  salary INTEGER NOT NULL,
+  dept_id INTEGER,
+  CONSTRAINT fk_department FOREIGN KEY (dept_id) REFERENCES departments(id) ON DELETE SET NULL
+);
+
+CREATE TABLE employees (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(20) NOT NULL,
+  last_name VARCHAR(20) NOT NULL,
+  role_id INTEGER,
+  manager VARCHAR(40),
+  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL
+);
--- index.js
+++ index.js
@@ -1,18 +1,41 @@
 const inquirer = require('inquirer');
 const db = require('./db/connection');
+const cTable = require('console.table');
 const {
   chooseAction,
   addDept,
   addRole,
   addEmployee,
   updateRole,
-  runItBack
+  newTask
 } = require('./lib/questions');
 
 function init () {
   inquirer.prompt(chooseAction)
     .then((answers) => {
-      console.log(answers);
+      switch(answers.action) {
+        case "View all departments":
+          viewDepts();
+          break;
+        case "View all roles":
+          viewRoles();
+          break;
+        case "View all employees":
+          viewEmployees();
+          break;
+        case "Add a department":
+          newDept();
+          break;
+        case "Add a role":
+          newRole();
+          break;
+        case "Add an employee":
+          newEmployee();
+          break;
+        case "Update an employee":
+          updateEmpRole();
+          break;
+      }
     })
     .catch((error) => {
       console.log(error);
@@ -20,7 +43,110 @@ function init () {
 };
 
 function newDept() {
-  console.log("test");
+  inquirer.prompt(addDept)
+    .then((body) => {
+      db.query(
+      'INSERT INTO departments (dept_name) VALUES (?)',
+      [body.dept], (error, result) => {
+        if (error) {
+          throw error;
+        } else {
+          runItBack();
+        }
+      });
+    });
+}
+
+function newRole() {
+  inquirer.prompt(addRole)
+    .then((body) => {
+      db.query(
+      'INSERT INTO roles (job_title, salary, dept_id) VALUES (?,?,?)',
+      [body.roleTitle, body.roleSalary, body.roleDept], (error, result) => {
+        if (error) {
+          throw error;
+        } else {
+          runItBack();
+        }
+      });
+    });
+}
+
+function newEmployee() {
+  inquirer.prompt(addEmployee)
+    .then((body) => {
+      db.query(
+        'INSERT INTO employees (first_name, last_name, role_id, manager) VALUES (?,?,?,?)',
+        [body.firstName, body.lastName, body.role, body.manager], (error, result) => {
+          if (error) {
+            throw error;
+          } else {
+            runItBack();
+          }
+        });
+    })
+}
+
+function viewDepts() {
+  db.query(
+    'SELECT * FROM departments',
+    (error, res) => {
+      if (error) {
+        throw error;
+      } else {
+        console.table(res)
+        runItBack();
+      }
+    }
+  )
+}
+
+function viewRoles() {
+  db.query(
+    'SELECT * FROM roles LEFT JOIN departments ON roles.dept_id = departments.id',
+    (error, res) => {
+      if (error) {
+        throw error;
+      } else {
+        console.table(res);
+        runItBack();
+      }
+    }
+  )
+}
+
+function viewEmployees() {
+  db.query(
+    'SELECT * FROM employees LEFT JOIN roles ON employees.role_id = roles.id LEFT JOIN departments ON roles.dept_id = departments.id',
+    (error, res) => {
+      if (error) {
+        throw error;
+      } else {
+        console.table(res);
+        runItBack();
+      }
+    }
+  )
+}
+
+function runItBack() {
+  inquirer.prompt(newTask)
+    .then((body) => {
+      if (body.confirm) {
+        init();
+      } else {
+        return;
+      }
+    });
 }
 
 init();
+
+// simple query
+// db.query(
+//   'SELECT * FROM `table` WHERE `name` = "Page" AND `age` > 45',
+//   function(err, results, fields) {
+//     console.log(results); // results contains rows returned by server
+//     console.log(fields); // fields contains extra meta data about results, if available
+//   }
+// );
